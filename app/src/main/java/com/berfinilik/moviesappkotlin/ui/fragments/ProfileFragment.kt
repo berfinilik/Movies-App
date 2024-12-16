7d95a8949df6daa28e8aa46dfe3363884637ac53
Berfin İlik <berfinnilik1074@gmail.com>
2024-12-16 14:13:51 +0300

    Kullanıcı çıkışı ve LoginActivity'ye yönlendirme eklendi.

--- app/src/main/java/com/berfinilik/moviesappkotlin/ui/fragments/ProfileFragment.kt
+++ app/src/main/java/com/berfinilik/moviesappkotlin/ui/fragments/ProfileFragment.kt
@@ -1,12 +1,16 @@
 package com.berfinilik.moviesappkotlin.ui.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
 import androidx.navigation.fragment.findNavController
 import com.berfinilik.moviesappkotlin.databinding.FragmentProfileBinding
+import com.berfinilik.moviesappkotlin.ui.activities.LoginActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class ProfileFragment : Fragment() {
 
@@ -22,7 +26,27 @@ class ProfileFragment : Fragment() {
             val action = ProfileFragmentDirections.actionProfileFragmentToSettingsFragment()
             findNavController().navigate(action)
         }
+        binding.logoutLayout.setOnClickListener {
+            showLogoutConfirmationDialog()
+        }
         return binding.root
+    }
+    private fun showLogoutConfirmationDialog() {
+        AlertDialog.Builder(requireContext())
+            .setTitle("Çıkış Yap")
+            .setMessage("Hesabınızdan çıkış yapmak istediğinize emin misiniz?")
+            .setPositiveButton("Evet") { _, _ ->
+                performLogout()
+            }
+            .setNegativeButton("Hayır", null)
+            .show()
+    }
+
+    private fun performLogout() {
+        FirebaseAuth.getInstance().signOut()
+        val intent = Intent(requireContext(), LoginActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        startActivity(intent)
     }
 
     override fun onDestroyView() {
